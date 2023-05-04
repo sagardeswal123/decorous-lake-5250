55689b8ae7d7914caa4ad05439494ec7164f5a59
Deepak1095 <ddhankhar999@gmail.com>
2023-05-04 23:41:15 +0530

    emi calculator

--- project/src/Pages/MainRoutes.jsx
+++ project/src/Pages/MainRoutes.jsx
@@ -3,13 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import  HomePage  from "./HomePage";
 import Login from './Login';
 import Register from './Register';
-
+import EmiCalculator from "./EmiCalculator"
 export const MainRoutes = () => {
   return <div>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="login" element={<Login />} />
       <Route path="login" element={<Register />} />
+      <Route path="/emiCalculator" element={<EmiCalculator />} />
     </Routes>
   </div>;
 };
