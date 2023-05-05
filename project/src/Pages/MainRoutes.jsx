f9385648f1b637967ba880cc6bff484f4f7343bd
amit <amityadav11@hotmail.com>
2023-05-05 12:47:20 +0530

    update

--- project/src/Pages/MainRoutes.jsx
+++ project/src/Pages/MainRoutes.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import  HomePage  from "./HomePage";
-import Login from './Login';
-import Register from './Register';
+import { Routes, Route, Switch } from "react-router-dom";
+import HomePage from "./HomePage";
+import Login from "./Login";
+import Register from "./Register";
 
-export const MainRoutes = () => {
-  return <div>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="login" element={<Login />} />
-      <Route path="login" element={<Register />} />
-    </Routes>
-  </div>;
+const MainRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
+    </div>
+  );
 };
+
+export default MainRoutes;
