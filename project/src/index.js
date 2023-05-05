66d9a247e33329faa0976b750882b15f9d92254f
amit <amityadav11@hotmail.com>
2023-05-05 12:49:18 +0530

    navMenu

--- project/src/index.js
+++ project/src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
-import customTheme from "./Context/Theme";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
