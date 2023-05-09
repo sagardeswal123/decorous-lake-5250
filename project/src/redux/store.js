d7bab12f48ce85f412680ace1086e9c1b32f4697
sagardeswal123 <sagardeswal798@gmail.com>
2023-05-09 13:41:35 +0530

    conflict resolved

--- project/src/redux/store.js
+++ project/src/redux/store.js
@@ -1,24 +1,19 @@
-import { legacy_createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { reducer as AuthReducer } from "./AuthReducer/reducer";
+import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
+import {reducer as PostDataReducer} from "./personalInformationRedux/reducer"
+import {reducer1 as getData} from "./personalInformationRedux/reducer"
 
 const rootReducer=combineReducers({
     AuthReducer,
+    PostDataReducer,
+    getData
 })
 
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
 
-import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
-import thunk from "redux-thunk";
-import {reducer as PostDataReducer} from "./personalInformationRedux/reducer"
-import {reducer1 as getData} from "./personalInformationRedux/reducer"
-const rootReducer = combineReducers({
-PostDataReducer,
-getData
 
-});
 
-export const store = legacy_createStore(rootReducer,applyMiddleware(thunk));
 
 
