d23cf057862fa53a430579c13805bbfc365f6811
Deepak1095 <ddhankhar999@gmail.com>
2023-07-19 20:39:28 +0530

    add media query and some style in EMI Calculator page

--- project/src/Pages/EmiCalculator.jsx
+++ project/src/Pages/EmiCalculator.jsx
@@ -1,4 +1,4 @@
-import { Box, Button } from '@chakra-ui/react'
+import { Box, Button,Text } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import { ChakraSlider } from "../Components/slider"
 import { Link, Navigate } from 'react-router-dom'
@@ -37,23 +37,23 @@ const EmiCalculator = () => {
         <Box fontSize={"20px"} >Calculate your EMI and choose the most suitable product for you</Box>
       </Box>
 
-      <Box height={"400px"} width="80%" margin="auto" marginBottom={"100px"} marginTop={"100px"}>
+      <Box  width="80%" margin="auto" marginBottom={"100px"} marginTop={"100px"} >
         <Box textAlign="center" display="flex" alignItems="center" justifyContent="center">EMI CALCULATOR</Box>
-        <Box display={"flex"}>
-          <Box width={"50%"}>
+        <Box   display={{base:"block",md:"flex"}} flexDirection={{base:"column",md:"row"}} >
+          <Box width={{ base:"100%", md:"50%"}}  display="flex" flexDirection="column" alignItems="center" justifyContent="center" >
             <ChakraSlider name={"loan amount"} amount="5000" width="70%" min="2000" max="50000" steps="2000" onChange={handleLoanAmount} />
             <ChakraSlider name={"Tenure"} amount="0.25" width="70%" min="0.25" max="3.5" steps="0.25" onChange={handleLoanTenure} />
             <ChakraSlider name={"Rate Of Interest"} amount="10.25" width="70%" min="10.25" max="30" steps="0.25" onChange={handleLoanRate} />
           </Box>
           <Box marginTop={"5%"} marginLeft={"10%"}>
-            <Box>Your Emi Amount</Box>
-            <Box fontSize={"30px"}>{emi}</Box>
-            <Box fontSize={"24px"}>Total Payment</Box>
-            <Box>{(emi * +loanTenure * 12).toFixed(0)}</Box>
-            <Box fontSize={"24px"}>Total Interest</Box>
-            <Box>{((emi * +loanTenure * 12) - loanAmount).toFixed(0)}</Box>
+            <Text  fontSize='2xl'>Your EMI Amount</Text >
+            <Text  fontSize='2xl'>{emi}</Text >
+            <Text  fontSize='2xl'>Total Payment</Text >
+            <Text  fontSize='2xl'>{(emi * +loanTenure * 12).toFixed(0)}</Text >
+            <Text  fontSize='2xl'>Total Interest</Text >
+            <Text  fontSize='2xl'>{((emi * +loanTenure * 12) - loanAmount).toFixed(0)}</Text >
             <Link to="/loanApplicationPage">
-            <Button fontSize={"24px"} margin={"10px 0px 40px 0px"} backgroundColor={"pink.300"} color={"white"} _hover={{ backgroundColor: "pink.200" }} >Apply Loan </Button> 
+            <Button size='lg' margin={"10px 0px 40px 0px"} backgroundColor={"pink.300"} color={"white"} _hover={{ backgroundColor: "pink.200" }} >Apply Loan </Button> 
             </Link>
         
           </Box>
