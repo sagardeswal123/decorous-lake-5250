5415499c8bd6755b4efd9320ff7c9d49ccb30fbc
Deepak1095 <ddhankhar999@gmail.com>
2023-07-22 01:43:05 +0530

    update chart

--- project/src/Pages/EmiCalculator.jsx
+++ project/src/Pages/EmiCalculator.jsx
@@ -6,8 +6,8 @@ import { Chart } from "react-google-charts";
 
 const EmiCalculator = () => {
   const [loanAmount, setLoanAmount] = useState(5000)
-  const [loanTenure, setLoanTenure] = useState(0.25)
-  const [loanRate, setLoanRate] = useState(10.25)
+  const [loanTenure, setLoanTenure] = useState(5.00)
+  const [loanRate, setLoanRate] = useState(12.25)
   const [emi, setEmi] = useState(0)
   const handleLoanAmount = (e) => {
     setLoanAmount(e)
@@ -31,64 +31,98 @@ const EmiCalculator = () => {
 
   const data = [
     ["Task", "Hours per Day"],
-    ["Principle",loanAmount],
-    ["Total Interest",Math.abs(emi*loanTenure*12-loanAmount) ],
+    ["Principle", loanAmount],
+    ["Total Interest", Math.abs(emi * loanTenure * 12 - loanAmount)],
   ];
 
-  const options = {
+  var options = {
     title: "All Details",
     pieHole: 0.5,
-    is3D: false,
+    // pieSliceText: 'value', 
+    height: 300,
+    width: "100%",
+    colors: ['#51defc', '#6af884',],
+    chartArea: {
+      left: 50,
+      top: 0,
+      right: 0,
+      bottom: 0,
+    }
   };
 
-
-
   return (
-    <div>
-
-      <Box textAlign="center" alignItems="center" justifyContent="center" height={"100px"} color="white" backgroundColor={"blue.300"} marginTop={"50px"} >
-        <Box fontSize={{ xl: '40px', lg: '35px', md: '30px', sm: '20px', base: '20px' }}>Personal Loan EMI Calculator</Box >
-        <Box fontSize={{  sm: '20px', base: '25px' }}  >Calculate your EMI and choose the most suitable product for you</Box>
+    <Box>
+      <Box textAlign="center" alignItems="center" justifyContent="center" color="white" bg={"linear-gradient(to right, #51defc, lightblue)"} marginTop={"2px"} padding={"5px"}  >
+        <Box fontSize={{ xl: '40px', lg: '35px', md: '30px', sm: '20px', base: '20px' }} fontFamily={"cursive"}>Personal Loan EMI Calculator</Box >
+        <Box fontSize={{ sm: '20px', base: '10px' }} fontFamily={"cursive"} >Calculate your EMI and choose the most suitable product for you</Box>
       </Box>
+      <Box width="90%" margin="auto" marginBottom={"100px"} marginTop={"40px"} >
+        <Text fontFamily={"cursive"} fontSize={"24px"} textAlign="center" display="flex" alignItems="center" justifyContent="center">EMI CALCULATOR</Text >
 
-      <Box width="90%" margin="auto" marginBottom={"100px"} marginTop={"100px"} >
-        <Box textAlign="center" display="flex" alignItems="center" justifyContent="center">EMI CALCULATOR</Box>
-        <Box display={{ base: "block", md: "flex" }} flexDirection={{ base: "column", md: "row" }}  >
-          <Box width={{ base: "100%", md: "90%"  }} display="flex" flexDirection="column" alignItems="center" justifyContent="center" >
-            <ChakraSlider name={"loan amount"} amount="5000" width={{base:"80%",md:"70%"}} min="2000" max="50000" steps="2000" onChange={handleLoanAmount} />
-            <ChakraSlider name={"Tenure"} amount="0.25" width={{base:"80%",md:"70%"}} min="0.25" max="3.5" steps="0.25" onChange={handleLoanTenure} />
-            <ChakraSlider name={"Rate Of Interest"} amount="10.25" width={{base:"80%",md:"70%"}} min="10.25" max="30" steps="0.25" onChange={handleLoanRate} />
+        <Box marginTop={"40px"}>
+          <Box display={{ base: "block", md: "flex" }} flexDirection={{ base: "column", md: "row" }}>
+            {/* emi calculator */}
+            <Box width={{ base: "100%", md: "90%" }} display="flex" flexDirection="column" alignItems="center" justifyContent="center" >
+              <ChakraSlider name={<Text fontFamily={"cursive"}>Loan amount</Text>} amount="5000" width={{ base: "80%", md: "70%" }} min="2000" max="50000" steps="2000" onChange={handleLoanAmount} />
+              <ChakraSlider name={<Text fontFamily={"cursive"}>Tenure</Text>} amount="5.00" width={{ base: "80%", md: "70%" }} min="0.25" max="20" steps="0.25" onChange={handleLoanTenure} />
+              <ChakraSlider name={<Text fontFamily={"cursive"}>Rate of Interest</Text>} amount="12.25" width={{ base: "80%", md: "70%" }} min="10.25" max="30" steps="0.25" onChange={handleLoanRate} />
+            </Box>
+            {/* Emi Chart */}
+            <Box marginTop={"3%"} width={"80%"}  >
+              <Chart
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center"
+                chartType="PieChart"
+                data={data}
+                options={options}
+              />
+            </Box>
           </Box>
-          <Box marginTop={"3%"}>
-            <Chart
-            width="100%"
-            display="flex" 
-            alignItems="center" 
-            justifyContent="center"
-              chartType="PieChart"
-              height="400px"
-              data={data}
-              options={options}
-            />
-          </Box>
-          <Box marginTop={"5%"} width={{base:"90%" , md:"40%"}}>
-            <Text fontSize='2xl'>Your EMI Amount</Text >
-            <Text fontSize='2xl'>{emi}</Text >
-            <Text fontSize='2xl'>Total Payment</Text >
-            <Text fontSize='2xl'>{(emi * +loanTenure * 12).toFixed(0)}</Text >
-            <Text fontSize='2xl'>Total Interest</Text >
-            <Text fontSize='2xl'>{((emi * +loanTenure * 12) - loanAmount).toFixed(0)}</Text >
-            <Link to="/loanApplicationPage">
-              <Button size='lg' margin={"10px 0px 40px 0px"} backgroundColor={"pink.300"} color={"white"} _hover={{ backgroundColor: "pink.200" }} >Apply Loan </Button>
-            </Link>
+          {/* About Emi */}
+         
+          <Box
+      width={{ base: '90%', md: '40%' }}
+      margin="auto"
+      marginTop={{ base: '0px', md: '40px' }}
+      padding="20px"
+      border="1px solid #ddd"
+      borderRadius="8px"
+      boxShadow="0 2px 4px rgba(0, 0, 0, 0.1)"
+    >
+      <Text fontFamily="cursive" fontSize="2xl" fontWeight="bold">
+        Your EMI Amount: {emi}
+      </Text>
+      <Text fontFamily="cursive" fontSize="xl">
+        Total Payment: {(emi * +loanTenure * 12).toFixed(0)}
+      </Text>
+      <Text fontFamily="cursive" fontSize="xl">
+        Total Interest: {((emi * +loanTenure * 12) - loanAmount).toFixed(0)}
+      </Text>
+      <Link to="/loanApplicationPage">
+        <Button
+          size="lg"
+          marginTop="20px"
+          backgroundColor="
+          #51defc"
+          color="white"
+          _hover={{ backgroundColor: "#139ebd" }}
+          _focus={{ boxShadow: "0 0 0 3px rgba(255, 192, 203, 0.5)" }}
+        >
+          Apply Loan
+        </Button>
+      </Link>
+    </Box>
+  
 
-          </Box>
+
         </Box>
       </Box>
 
 
 
-    </div>
+    </Box>
   )
 }
 
